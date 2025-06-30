d38bde6577de9ee10802b4dad0f9e1f3f16c644b
Swiftz007 <cxllme14zx@gmail.com>
2025-06-30 21:29:46 +0700

    SwiftzRandom2.lua

--- SwiftzRandom2.lua
+++ SwiftzRandom2.lua
@@ -38,15 +38,3 @@ RunService.RenderStepped:Connect(function()
 	local progress = math.clamp(elapsed / duration, 0, 1)
 	title.TextTransparency = 1 - progress
 end)
-
-task.wait(duration)
-
--- Fade out UI
-TweenService:Create(title, TweenInfo.new(1), {TextTransparency = 1}):Play()
-TweenService:Create(bg, TweenInfo.new(1), {BackgroundTransparency = 1}):Play()
-
-task.wait(1.2)
-screenGui:Destroy()
-
--- ⏬ Run External Script
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mwrtin/Wow/refs/heads/main/ee0a8186b86e65dc8d66206d9045e89e%20(1).txt"))()
